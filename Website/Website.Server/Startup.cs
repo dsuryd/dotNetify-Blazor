b271f482514a0b5541357c66c24cfb0cfb9c5858
Dicky Suryadi <dsuryd@gmail.com>
2020-06-16 16:27:29 -0700

    Add dotnetify-pulse.

--- Website/Website.Server/Startup.cs
+++ Website/Website.Server/Startup.cs
@@ -1,8 +1,10 @@
 using DotNetify;
+using DotNetify.Pulse;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace Website.Server
 {
@@ -13,31 +15,39 @@ namespace Website.Server
       public void ConfigureServices(IServiceCollection services)
       {
          services.AddCors(options => options.AddPolicy(name: "CorsPolicy", builder => builder
-            .WithOrigins("http://localhost:8080")  // Website.Client URL.
+            .SetIsOriginAllowed(_ => true)
             .AllowAnyHeader()
             .AllowAnyMethod()
             .AllowCredentials()));
 
-         services.AddMemoryCache();
          services.AddSignalR();
          services.AddDotNetify();
+         services.AddDotNetifyPulse();
 
+         services.AddLogging();
          services.AddTransient<ILiveDataService, MockLiveDataService>();
          services.AddScoped<ICustomerRepository, CustomerRepository>();
       }
 
       // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-      public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+      public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> logger)
       {
          app.UseCors("CorsPolicy");
-         app.UseWebSockets();
 
+         app.UseWebSockets();
          app.UseDotNetify(config =>
         {
-           config.UseDeveloperLogging();
+           config.UseDeveloperLogging(log =>
+           {
+              if (!log.Contains("vmId=PulseVM"))
+                 logger.LogInformation(log);
+           });
            config.RegisterAssembly(GetType().Assembly);
            config.RegisterAssembly("DotNetify.DevApp.ViewModels");
         });
+
+         // Real-time logging: http://localhost:8090/blazor/pulse.
+         app.UseDotNetifyPulse();
 
          if (env.IsDevelopment())
          {
