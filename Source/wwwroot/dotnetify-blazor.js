f98627c735e20504568cbc37d2532c90ce44ba5c
Dicky Suryadi <dsuryd@gmail.com>
2020-10-24 22:12:13 -0700

    Fix 'circular reference' console error.

--- Source/wwwroot/dotnetify-blazor.js
+++ Source/wwwroot/dotnetify-blazor.js
@@ -1,76 +1,78 @@
 dotnetify_blazor = {
-  version: 1.0,
-  _eventListeners: [],
-  addEventListener: function (event, elem, callbackHelper) {
-    if (typeof elem === 'string') elem = document.querySelector(elem);
-    if (!(elem && typeof elem.addEventListener === 'function')) throw "Cannot listen to event '" + event + "': invalid VMContext @ref.";
+    version: 1.0,
+    _eventListeners: [],
+    addEventListener: function (event, elem, callbackHelper) {
+        if (typeof elem === 'string') elem = document.querySelector(elem);
+        if (!(elem && typeof elem.addEventListener === 'function')) throw "Cannot listen to event '" + event + "': invalid VMContext @ref.";
 
-    // Blazor component inside React container must be temporarily moved out until the container is rendered,
-    // otherwise it will re-rendered as React children and lose its association with the Blazor app.
-    if (elem.getAttribute('vm') && event === 'onStateChange') {
-      const insideContainer = () => {
-        let parent = elem.parentElement;
-        while (parent) {
-          if (parent._isContainer) return parent;
-          parent = parent.parentElement;
+        // Blazor component inside React container must be temporarily moved out until the container is rendered,
+        // otherwise it will re-rendered as React children and lose its association with the Blazor app.
+        if (elem.getAttribute('vm') && event === 'onStateChange') {
+            const insideContainer = () => {
+                let parent = elem.parentElement;
+                while (parent) {
+                    if (parent._isContainer) return parent;
+                    parent = parent.parentElement;
+                }
+            };
+            const parent = insideContainer();
+            if (parent) {
+                const vmId = elem.getAttribute('vm');
+                const elemParent = elem.parentElement;
+                const tempPlaceholderId = '__tmp_' + vmId;
+                const tempPlaceholder = document.createElement('span');
+                const tempLocation = document.createElement('span');
+                const tempParent = document.body.appendChild(tempLocation);
+                const restore = () => {
+                    const placeholder = document.getElementById(tempPlaceholderId);
+                    if (placeholder) {
+                        while (tempParent.childNodes.length > 0) placeholder.parentElement.appendChild(tempParent.childNodes[0]);
+                        document.body.removeChild(tempParent);
+                        placeholder.parentElement.removeChild(placeholder);
+                    }
+                    elem.setAttribute('vm', vmId);
+                    return true;
+                };
+
+                tempPlaceholder.setAttribute('id', tempPlaceholderId);
+                tempLocation.style.display = 'none';
+                elem.setAttribute('vm', '');
+                while (elemParent.childNodes.length > 0) tempParent.appendChild(elemParent.childNodes[0]);
+                elemParent.appendChild(tempPlaceholder);
+
+                let moved = false;
+                const restorePosition = () => {
+                    moved = moved || restore();
+                    parent.removeEventListener('mounted', restorePosition);
+                };
+                parent.addEventListener('mounted', restorePosition);
+                setTimeout(restorePosition, 750);
+            }
         }
-      };
-      const parent = insideContainer();
-      if (parent) {
-        const vmId = elem.getAttribute('vm');
-        const elemParent = elem.parentElement;
-        const tempPlaceholderId = '__tmp_' + vmId;
-        const tempPlaceholder = document.createElement('span');
-        const tempLocation = document.createElement('span');
-        const tempParent = document.body.appendChild(tempLocation);
-        const restore = () => {
-          const placeholder = document.getElementById(tempPlaceholderId);
-          if (placeholder) {
-            while (tempParent.childNodes.length > 0) placeholder.parentElement.appendChild(tempParent.childNodes[0]);
-            document.body.removeChild(tempParent);
-            placeholder.parentElement.removeChild(placeholder);
-          }
-          elem.setAttribute('vm', vmId);
-          return true;
+
+        const callback = e => {
+            callbackHelper.invokeMethodAsync('Callback', JSON.stringify(e.detail, 
+                // Don't stringify DOM elements.
+                (_, value) => value instanceof HTMLElement ? value.id : value
+            ));
         };
-
-        tempPlaceholder.setAttribute('id', tempPlaceholderId);
-        tempLocation.style.display = 'none';
-        elem.setAttribute('vm', '');
-        while (elemParent.childNodes.length > 0) tempParent.appendChild(elemParent.childNodes[0]);
-        elemParent.appendChild(tempPlaceholder);
-
-        let moved = false;
-        const restorePosition = () => {
-          moved = moved || restore();
-          parent.removeEventListener('mounted', restorePosition);
-        };
-        parent.addEventListener('mounted', restorePosition);
-        setTimeout(restorePosition, 750);
-      }
+        if (!dotnetify_blazor._eventListeners.some(x => x.elem === elem && x.event === event)) {
+            dotnetify_blazor._eventListeners.push({ elem, event, remove: () => elem.removeEventListener(event, callback) });
+            elem.addEventListener(event, callback);
+        }
+    },
+    configure: function (config) {
+        dotnetify.debug = config.debug;
+        if (config.hubServerUrl) dotnetify.hubServerUrl = config.hubServerUrl;
+    },
+    dispatch: function (elem, state) {
+        if (!(elem && elem.vm)) throw 'Invalid VMContext @ref.';
+        const stateJson = JSON.parse(state);
+        Object.assign(elem.state, stateJson);
+        elem.vm.$dispatch(stateJson);
+    },
+    removeAllEventListeners: function (elem) {
+        dotnetify_blazor._eventListeners.filter(x => x.elem === elem).forEach(x => x.remove());
+        dotnetify_blazor._eventListeners = dotnetify_blazor._eventListeners.filter(x => x.elem !== elem);
     }
-
-    const callback = e => {
-      // Ignore expected exception from input elements emitting its DOM ref.
-      if (e.detail.eventName !== 'onInputRef') callbackHelper.invokeMethodAsync('Callback', JSON.stringify(e.detail));
-    };
-    if (!dotnetify_blazor._eventListeners.some(x => x.elem === elem && x.event === event)) {
-      dotnetify_blazor._eventListeners.push({ elem, event, remove: () => elem.removeEventListener(event, callback) });
-      elem.addEventListener(event, callback);
-    }
-  },
-  configure: function (config) {
-    dotnetify.debug = config.debug;
-    if (config.hubServerUrl) dotnetify.hubServerUrl = config.hubServerUrl;
-  },
-  dispatch: function (elem, state) {
-    if (!(elem && elem.vm)) throw 'Invalid VMContext @ref.';
-    const stateJson = JSON.parse(state);
-    Object.assign(elem.state, stateJson);
-    elem.vm.$dispatch(stateJson);
-  },
-  removeAllEventListeners: function (elem) {
-    dotnetify_blazor._eventListeners.filter(x => x.elem === elem).forEach(x => x.remove());
-    dotnetify_blazor._eventListeners = dotnetify_blazor._eventListeners.filter(x => x.elem !== elem);
-  }
 };
